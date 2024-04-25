d992a3ca3c7a0a7545830564ae0766b1e8bf1bc5
ajay <krishajay.g@gmail.com>
2024-04-25 08:47:27 -0400

    Formatting

--- Misc_Techniques/detect_G325A.py
+++ Misc_Techniques/detect_G325A.py
@@ -1,8 +1,14 @@
 def detect_g325av5(text):
+    ''' 
+    Note: This code is written on a bunch of rules assuming the OCR is accurate AND that the ocr outputs are all concatenated
+    Better algorithms can be used
+    '''
     if "G-325" in text:
         if len(text) >= 1980 and len(text) < 3300:
-            if ("G-325A" in text[:400] or "G-325A" in text[-200:] or "G-325 A" in text[:400] or "G-325 A" in text[-200:] or "325" in text[-200:]) \
-            and ("I-130" not in text[-200:] and "Form I" not in text[-200:] and "Form 1" not in text[-200:]):
+            if ("G-325A" in text[:400] or "G-325A" in text[-200:] \
+                 or "G-325 A" in text[:400] or "G-325 A" in text[-200:] \
+                 or "325" in text[-200:]) \
+                 and ("I-130" not in text[-200:] and "Form I" not in text[-200:] and "Form 1" not in text[-200:]):
                 return 1
             else:
                 return 0 
